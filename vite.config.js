02bb0197f20f521e8db33995c01d363de7ab849d
Jose Vigo <josevigodev@gmail.com>
2025-06-13 11:54:43 -0700

    ss

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/modmania-landing-page/',
   plugins: [react(), tailwindcss()],
 })
